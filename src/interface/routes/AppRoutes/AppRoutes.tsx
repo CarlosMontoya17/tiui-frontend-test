6153b0b147145a5ce306531a71caf3159f4fdd8a
Carlos Montoya <98224572+CarlosMontoya17@users.noreply.github.com>
2024-07-01 20:00:09 -0600

    [update] add task context

--- src/interface/routes/AppRoutes/AppRoutes.tsx
+++ src/interface/routes/AppRoutes/AppRoutes.tsx
@@ -1,13 +1,16 @@
 "use client";
+import TaskContext from '@/context/TaskContext/TaskContext';
 import { Home } from '@/interface/pages';
 import React from 'react';
 import { Route, Routes } from 'react-router-dom';
 
 const AppRoutes: React.FC<any>  = () => {
 	return (
-		<Routes>
-			<Route path='/' element={<Home />}></Route>
-		</Routes>
+		<TaskContext>
+			<Routes>
+				<Route path='/' element={<Home />}></Route>
+			</Routes>
+		</TaskContext>
 	);
 };
 
