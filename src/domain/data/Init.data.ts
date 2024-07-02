c3acf4f85c90303ec01684c7c67a6a09e95e3e3c
Carlos Montoya <98224572+CarlosMontoya17@users.noreply.github.com>
2024-07-02 00:25:59 -0600

    [fix] spanish task initial

--- src/domain/data/Init.data.ts
+++ src/domain/data/Init.data.ts
@@ -3,8 +3,8 @@ import { ITask } from "../entities";
 export const Items: ITask[] = [
     {
         id: 1,
-        title: "Test item",
-        text: "Description of the activity",
+        title: "Tarea de prueba",
+        text: "Descripción de la actividad [opcional]",
         status: false,
         date: "2024-06-30T12:36:21.502Z"
     }
