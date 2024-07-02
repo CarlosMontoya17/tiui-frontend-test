ed393ee0e5a9ee0bf56ad02d638a62cafcd228d1
Carlos Montoya <98224572+CarlosMontoya17@users.noreply.github.com>
2024-07-01 18:36:54 -0600

    [feature] date util

---
+++ src/utils/DateFormat.util.ts
@@ -0,0 +1,15 @@
+export const formatDate = (string: string, format: string = "yyyy/mm/dd HH:mm") => {
+    const date = new Date(string);
+    const year = date.getFullYear().toString();
+    const month = (date.getMonth() + 1).toString().padStart(2, '0');
+    const day = date.getDate().toString().padStart(2, '0');
+    const hours = date.getHours().toString().padStart(2, '0');
+    const minutes = date.getMinutes().toString().padStart(2, '0');
+    const formattedDate = format
+        .replace('yyyy', year)
+        .replace('mm', month)
+        .replace('dd', day)
+        .replace('HH', hours)
+        .replace('mm', minutes);
+    return formattedDate;
+};
