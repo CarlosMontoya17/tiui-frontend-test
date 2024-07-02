e1f80c60c7e147d397caabd15c2c1f080f91ec79
Carlos Montoya <98224572+CarlosMontoya17@users.noreply.github.com>
2024-07-01 18:38:13 -0600

    [update] main structure

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,19 @@
+import { Provider } from 'react-redux'
 import './App.css'
+import AppStore from './infraestructure/store/App.store';
+import { BrowserRouter as Router } from 'react-router-dom';
+import { AppRoutes } from './interface/routes';
+import Navbar from './interface/components/templates/Navbar/Navbar';
 
 function App() {
   return (
-    <>
-      Hello
-    </>
+    <Provider store={AppStore}>
+      <Router>
+        <Navbar>
+          <AppRoutes />
+        </Navbar>
+      </Router>
+    </Provider>
   )
 }
 
