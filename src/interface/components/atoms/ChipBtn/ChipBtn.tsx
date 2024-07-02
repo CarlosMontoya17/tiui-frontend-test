4419bce6ef51d3fc92fafbd24ac0cf1ba8e91382
Carlos Montoya <98224572+CarlosMontoya17@users.noreply.github.com>
2024-07-01 18:35:26 -0600

    [feature] chip button atom

---
+++ src/interface/components/atoms/ChipBtn/ChipBtn.tsx
@@ -0,0 +1,32 @@
+"use client";
+import { styled, useTheme } from '@mui/system';
+import React from 'react';
+
+
+
+const CustomChipBtn = styled('button')(
+	() => 
+`
+    width: fit-content;
+    padding: 0.25rem 0.5rem;
+    border-radius: 0.375rem;
+    font-weight: 600;
+    font-size: 0.85rem;
+`);
+
+export type ChipBtnProps = {
+	text: string;
+	active: boolean;
+	onClick: () => any;
+}
+
+const ChipBtn: React.FC<ChipBtnProps>  = ({ text, active, onClick }) => {
+	const primary = useTheme().palette.primary.light;
+	return (
+		<CustomChipBtn onClick={onClick} style={active ? { backgroundColor: primary } : {} }>
+			{text}
+		</CustomChipBtn>
+	);
+};
+
+export default ChipBtn;
