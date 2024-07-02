c33812382bc44475e3f138e9d1f533215f058a1a
Carlos Montoya <98224572+CarlosMontoya17@users.noreply.github.com>
2024-07-01 18:34:23 -0600

    [feature] initial data

---
+++ src/domain/data/Init.data.ts
@@ -0,0 +1,11 @@
+import { ITask } from "../entities";
+
+export const Items: ITask[] = [
+    {
+        id: 1,
+        title: "Test item",
+        text: "Description of the activity",
+        status: false,
+        date: "2024-06-30T12:36:21.502Z"
+    }
+];
